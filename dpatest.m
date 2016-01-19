7608dcf7e2f3ac6a2be49495ae4fa211ebdfe562
vergi40 <teemu.laine@student.tut.fi>
2016-01-19 11:28:40 +0100

    hamming weight is probably just this

--- dpatest.m
+++ dpatest.m
@@ -26,27 +26,39 @@ L = 1000;
 plot(1:L, trace1(1:L), 'r', (1:L), trace2(1:L), 'b');
 
 
-% Build a matrix of hypothetical intermediate values inside the cipher for all possible keys and traces
-% create power hypothesis for each byte of the key
-% correlate that with the power traces to extract key
-amountoftraces = 100; % up to 10000
+% POWER MODEL
+% Assume high power consumption if the Hamming
+% Weight of the output of the first Subbox is high and vice versa
+% -> output of first subbox = output of first SubBytes
+% ---> "The Hamming weight of a string is the number of symbols 
+% ---> that are different from the zero-symbol of the alphabet used."
 
-% do for every byte
-for byte = 1:16
-    
-    % 256 possible bytes
-    powerHyphotesis = zeros(amountoftraces,256);
-    for keycandidate = 0:255                            
-        % create power hyphothesis
-        
-    end;
-    % function mycorr returns the correlation coeficients matrix calculated
-    % from the power consumption hypothesis matrix powerHypothesis and the
-    % measured power traces. The resulting correlation coeficients stored in
-    % the matrix CC are later used to extract the correct key.
-    
-    % Calculate correlation coefficients matrix by using power consumption hypothesis matrix 
-    % powerhyphotesis and traces. Save to new matrix and use it to extract correct key
-    
+% Key is right if the predicted power consumption is
+% retrievable from the provided power traces
 
-end;
+
+
+% AddRoundKey here
+
+% SubBytes here
+
+% to be the subbox answer
+testnumber = 10;
+answer = aes_ct(1:testnumber,:);
+hammingWeight = sum(answer(:)!=0)
+
+
+
+
+% http://kutylowski.im.pwr.wroc.pl/articles/hamming.pdf
+%It is required for the attack to determine the total Hamming weights of
+%the output of the Feistel function of DES during the last round in a series of
+%encryptions. This weight consists of weight of the output of an S-Box S we
+%2
+%are attacking and the output of the remaining S-boxes. For finding the key one
+%guesses the subkey bits which go into the XOR gate immediately before S. Then
+%it is possible to compute the (hypothetical) outputs of S for the ciphertexts
+%at hand. If the subkey bits were guessed correctly, then there is a statistical
+%correlation between the weights of the computed outputs of S and the Hamming
+%weights measured. For a wrong guess, there is no such a correlation. This is
+%a way to determine which guess is correct and thereby to derive the key bits.%
